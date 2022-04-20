0d646a4e04ef94e028a614ed5f4e37d4adb7ba8e
Anna Nachkepiia <admin@MacBook-Pro-AnnaNachkepiia.local>
2022-04-20 13:34:39 +0300

    новая ветка

--- scripts/index.js
+++ scripts/index.js
@@ -25,61 +25,11 @@ import {
     formObj,
     editProfileValidator,
     addCardValidator
-} from './Consts.js'
+} from '../../scripts/Consts.js'
 
-import { Card } from './Card.js'
-
-// // Переменные
-// const popupEditElement = document.querySelector(".popup_type_edit");
-// const popupAddElement = document.querySelector(".popup_type_add-card");
-// const popupImgElement = document.querySelector(".popup_type_open-image");
-// const popupOpenAddButttonElement = document.querySelector(".profile__add-button");
-// const popupEditCloseButtonElement =
-//     document.querySelector(".popup__close_edit");
-// const popupAddCloseButtonElement = document.querySelector(".popup__close_add");
-// const popupImgCloseButtonElement = document.querySelector(".popup__close_img");
-// const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-// const popupSubmitButtonElement = popupEditElement.querySelector(
-//     ".popup__submit_type_save"
-// );
-// const popupSubmitButtonAdd = popupAddElement.querySelector('.popup__submit_type_create');
-
-// const popupFormElement = popupEditElement.querySelector(
-//     ".popup__form-container_type_edit"
-// );
-// const popupAddFormElement = popupAddElement.querySelector(
-//     ".popup__form-container_type_add"
-// );
-// const nameInput = popupFormElement.querySelector(".popup__edit-form_type_name");
-// const jobInput = popupFormElement.querySelector(".popup__edit-form_type_about");
-// const cardNameInput = popupAddElement.querySelector(
-//     ".popup__edit-form_type_card-name"
-// );
-// const cardLinkInput = popupAddElement.querySelector(
-//     ".popup__edit-form_type_link"
-// );
-// const profileTitle = document.querySelector(".profile__title");
-// const profileSubtitle = document.querySelector(".profile__subtitle");
-// const popupPicture = document.querySelector(".popup__picture");
-// const popupOpenPicture = document.querySelector(".popup__fulled-img");
-// const popupOpenPictureCapture = popupPicture.querySelector(
-//     ".popup__picture-title"
-// );
-
-// const listCards = document.querySelector(".places");
-
-// const formObj = {
-//     formSelector: '.popup__form-container',
-//     inputSelector: '.popup__edit-form',
-//     submitButtonSelector: '.popup__submit',
-//     submitButtonDisabledClass: 'popup__submit-disabled',
-//     errorClass: 'popup__edit-form_type_error',
-//     errorClassActive: 'popup__text-error_active'
-// }
+import { Card } from '../../scripts/Card.js'
 
 
-// const editProfileValidator = new FormValidator(formObj, popupFormElement);
-// const addCardValidator = new FormValidator(formObj, popupAddFormElement);
 
 editProfileValidator.enableValidation();
 addCardValidator.enableValidation();
